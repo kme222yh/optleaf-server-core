802dbc3d50f1f8805c56d1927c9ec010849dc82a
kme222yh <49933526+kme222yh@users.noreply.github.com>
2022-06-22 10:48:20 +0900

    validator notblank

--- validator/validator.go
+++ validator/validator.go
@@ -1,45 +1,44 @@
 package validator
+
 // ref : https://qiita.com/syoimin/items/b3923fea6070b0a3df8f
 // ref : https://github.com/go-playground/validator
 
-
 import (
-    "regexp"
-    "github.com/go-playground/validator/v10"
-)
+	"regexp"
 
+	"github.com/go-playground/validator/v10"
+)
 
 var (
-    validate = validator.New()
+	validate = validator.New()
 )
 
-
-
-
 func Run(m interface{}) error {
-    validate.RegisterValidation("include_low", includeLowercase)
-    validate.RegisterValidation("include_up", includeUppercase)
-    validate.RegisterValidation("include_num", includeNumeric)
-    return validate.Struct(m)
+	validate.RegisterValidation("notblank", notBlank)
+	validate.RegisterValidation("include_low", includeLowercase)
+	validate.RegisterValidation("include_up", includeUppercase)
+	validate.RegisterValidation("include_num", includeNumeric)
+	return validate.Struct(m)
 }
 
-
-
-
+// 空白で無いかどうか
+func notBlank(fl validator.FieldLevel) bool {
+	return len(fl.Field().String()) > 0
+}
 
 // 小文字が含まれるかどうか
 func includeLowercase(fl validator.FieldLevel) bool {
-    return checkRegexp("[a-z]", fl.Field().String())
+	return checkRegexp("[a-z]", fl.Field().String())
 }
 
 // 大文字が含まれるかどうか
 func includeUppercase(fl validator.FieldLevel) bool {
-    return checkRegexp("[A-Z]", fl.Field().String())
+	return checkRegexp("[A-Z]", fl.Field().String())
 }
 
 // 数値が含まれるかどうか
 func includeNumeric(fl validator.FieldLevel) bool {
-    return checkRegexp("[0-9]", fl.Field().String())
+	return checkRegexp("[0-9]", fl.Field().String())
 }
 
 // 特殊記号が含まれるかどうか
@@ -52,6 +51,6 @@ func includeNumeric(fl validator.FieldLevel) bool {
 
 // 正規表現共通関数
 func checkRegexp(reg, str string) bool {
-    r := regexp.MustCompile(reg).Match([]byte(str))
-    return r
+	r := regexp.MustCompile(reg).Match([]byte(str))
+	return r
 }
